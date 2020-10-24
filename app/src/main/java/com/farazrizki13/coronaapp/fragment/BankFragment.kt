abc58b1ae9e7ea7ff004e5694334549a993a07c8
Rizki Ramadhan <farazrizki13@gmail.com>
2020-10-24 10:47:33 +0700

    menambahkan beberapa function

--- app/src/main/java/com/farazrizki13/coronaapp/fragment/BankFragment.kt
+++ app/src/main/java/com/farazrizki13/coronaapp/fragment/BankFragment.kt
@@ -1,11 +1,16 @@
 package com.farazrizki13.coronaapp.fragment
 
+import android.graphics.SurfaceTexture
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
+import android.view.TextureView
 import android.view.View
 import android.view.ViewGroup
 import com.farazrizki13.coronaapp.R
+import kotlinx.android.synthetic.main.fragment_bank.*
+
 class BankFragment : Fragment() {
 
     override fun onCreateView(
@@ -15,6 +20,32 @@ class BankFragment : Fragment() {
         // Inflate the layout for this fragment
         val view = inflater.inflate(R.layout.fragment_bank, container, false)
         return view
+    }
+
+    private val surfaceListener = object : TextureView.SurfaceTextureListener {
+        override fun onSurfaceTextureSizeChanged(p0: SurfaceTexture, p1: Int, p2: Int) {
+        }
+
+        override fun onSurfaceTextureUpdated(surface: SurfaceTexture) = Unit
+
+        override fun onSurfaceTextureDestroyed(surface: SurfaceTexture) = true
+
+        override fun onSurfaceTextureAvailable(p0: SurfaceTexture, p1: Int, p2: Int) {
+            Log.d(TAG, "Textsurface width : $p1, height : $p2")
+            openCamera()
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        if (previewTextureView.isAvailable)
+            openCamera()
+        else
+            previewTextureView.surfaceTextureListener = surfaceListener
+    }
+
+    private fun openCamera() {
+
     }
 
     companion object {
